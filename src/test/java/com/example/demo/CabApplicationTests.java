53b31a4e3df6ff86b4de5ecb8a2e9184d6123862
vicky-sai <vicky.sss.1997@gmail.com>
2019-09-04 15:15:21 +0530

    initial commit

--- src/test/java/com/example/demo/CabApplicationTests.java
+++ src/test/java/com/example/demo/CabApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class BookApplicationTests {
+public class CabApplicationTests {
 
 	@Test
 	public void contextLoads() {
