8041d56bf871a62c28ddb9d42b1de2ab4fe40e07
vicky-sai <vicky.sss.1997@gmail.com>
2019-09-05 09:32:17 +0530

    initial reference - 2nd updated model

--- src/main/java/com/example/demo/model/DriverModel.java
+++ src/main/java/com/example/demo/model/DriverModel.java
@@ -2,11 +2,14 @@ package com.example.demo.model;
 
 import java.util.Date;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
@@ -64,4 +67,8 @@ public class DriverModel {
     
     @NotEmpty
     private String licenseNumber; //Eg. MH-4089503248
+    
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "geoLocation", referencedColumnName = "id")
+    private CurrentLocation location;
 }
